fd6fc01f88660a30da040b7f9c00c841b4a6fb1d
Nobody22 <leonforam@hotmail.co.uk>
2017-08-18 15:27:15 +0100

    Added deck shuffle
    7 shuffles to ensure a true shuffle maybe

--- Cards/Cards.py
+++ Cards/Cards.py
@@ -9,18 +9,3 @@ class Card():
     def get_card(self):
         return ("The " + self.value + " of " + self.suit)
 
-def create_deck():
-    suits = ["Hearts","Clubs","Diamonds","Spades"]
-    values = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-    #create deck
-    deck = []
-    for suit in suits:
-        for value in values:
-            card = Card(suit,value)
-            deck.append(card)
-    return deck
-
-if __name__ == '__main__':
-    deck = create_deck()
-    for card in deck:
-        print(card.get_card())
---
+++ Cards/Main.py
@@ -0,0 +1,32 @@
+import Cards
+import random
+
+
+def create_deck():
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    deck = []
+    #create deck
+
+    for suit in suits:
+        for value in values:
+            card = Cards.Card(suit, value)
+            deck.append(card)
+    return deck
+
+def shuffle(deck):
+    shuffled = []
+    print("shuffle")
+    while deck != []:
+        rand = random.randint(0, len(deck)-1)
+        shuffled.append(deck[rand])
+        deck.pop(rand)
+    return shuffled
+
+if __name__ == '__main__':
+    deck = create_deck()
+    for i in range(7):
+        deck = shuffle(deck)
+
+    for card in deck:
+        print(card.get_card())
